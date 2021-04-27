a8fb0e4ce91b32753b5bb7f6cbd297e92ae203ee
unknown <umarbennin1992@gmail.com>
2021-04-27 14:22:58 +0000

    s

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Col} from "react-bootstrap";
+
+const RegisterUserScreen = () => {
+    return (
+        <div>
+        <Col xs={12} sm={12} md={6} lg={6}>
+            <form></form>
+
+        </Col>
+        </div>
+    )
+}
+export default RegisterScreen
